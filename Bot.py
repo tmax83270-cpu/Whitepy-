fe8d150e55529ef02b2c4e1bc2776bc3c8c1d621
tmax83270-cpu <tmax83270@gmail.com>
2025-12-01 16:29:47 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,13 +1,19 @@
-import telebot
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Ton token BotFather
-TOKEN = os.environ.get("8476960807:AAGLf9Fy05l3A390iBjdigCNOYwtWNnVC0k")  # ou remplace directement par ton token en texte
+# Récupère le token depuis les variables d'environnement Fly.io
+TOKEN = os.environ.get("TOKEN")
 
-bot = telebot.TeleBot(TOKEN)
+# Fonction appelée quand quelqu'un envoie /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bonjour ! Ton bot fonctionne 😄")
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "Bonjour ! Ton bot fonctionne 😄")
+# Créer l'application du bot
+app = ApplicationBuilder().token(TOKEN).build()
 
-bot.infinity_polling()
+# Ajouter le handler pour la commande /start
+app.add_handler(CommandHandler("start", start))
+
+# Lancer le bot en continu
+app.run_polling()
