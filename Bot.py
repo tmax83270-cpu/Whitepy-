101c52113cca9928df71ff8213c960a8a4943eea
tmax83270-cpu <tmax83270@gmail.com>
2025-12-01 16:24:17 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,18 +1,13 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import telebot
+import os
 
-# Remplace par ton token BotFather
-TOKEN = "8476960807:AAGLf9Fy05l3A390iBjdigCNOYwtWNnVC0k"
+# Ton token BotFather
+TOKEN = os.environ.get("8476960807:AAGLf9Fy05l3A390iBjdigCNOYwtWNnVC0k")  # ou remplace directement par ton token en texte
 
-# Fonction appelée quand quelqu'un envoie /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bonjour ! Ton bot fonctionne 😄")
+bot = telebot.TeleBot(TOKEN)
 
-# Créer l'application du bot
-app = ApplicationBuilder().token(TOKEN).build()
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Bonjour ! Ton bot fonctionne 😄")
 
-# Ajouter le handler pour la commande /start
-app.add_handler(CommandHandler("start", start))
-
-# Lancer le bot en continu
-app.run_polling()
+bot.infinity_polling()
